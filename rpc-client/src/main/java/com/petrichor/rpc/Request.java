a8ae1db3e9c0cdcdce29c4a2c9f7acf635d67524
chenhao <1303720575@qq.com>
2020-08-07 18:58:38 +0800

    完善服务端handler方法

--- rpc-client/src/main/java/com/petrichor/rpc/Request.java
+++ rpc-client/src/main/java/com/petrichor/rpc/Request.java
@@ -14,9 +14,9 @@ import java.io.Serializable;
 public class Request implements Serializable {
     private static final long serialVersionUID = 1905122041950251207L;
     private String requestId;
-    private String InterfaceName;
-    private String menthodName;
-    private Object[] parameters;
-    //一个Class实例包含了该class的所有完整信息：
-    private Class<?>[] paramTypes;
+    private String InterfaceName;     //接口名
+    private String menthodName;        //具体调用的那个方法
+    private Object[] parameters;        //参数：实体类
+    //一个Class实例包含了该class的所有完整信息
+    private Class<?>[] paramTypes;      //参数类型，实体类的类信息
 }
--- rpc-server/src/main/java/com/petrichor/rpc/RpcResquestHandler.java
+++ rpc-server/src/main/java/com/petrichor/rpc/RpcResquestHandler.java
@@ -2,6 +2,9 @@ package com.petrichor.rpc;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @author petrichor
  * @date 2020/8/5 21:50
@@ -9,7 +12,33 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class RpcResquestHandler {
+    private static ServiceProvider serviceProvider = new ServiceProviderImpl();
     Object handle(Request rpcRequest){
-        return null;
+        String serviceName = rpcRequest.getInterfaceName();
+        //获取到请求的service实体
+        Object service = serviceProvider.getServiceProvider(serviceName);
+        return invokeTargetMethod(rpcRequest, service);
     }
+
+    /**
+     * handle 调用这个方法对客户端请求的某个方法，执行后返回
+     * @param rpcRequest 请求体
+     * @param service   具体哪个服务
+     * @return 方法执行结果
+     */
+    private Object invokeTargetMethod(Request rpcRequest, Object service) {
+        Object result = null;
+
+        try {
+            //从service根据方法名和参数列表获取到这个方法
+            Method method = service.getClass().getMethod(rpcRequest.getMenthodName(),rpcRequest.getParamTypes());
+            //方法被代理后，进行执行，返回结果
+            result = method.invoke(service,rpcRequest.getParameters());
+        } catch (NoSuchMethodException |IllegalAccessException|InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
 }
